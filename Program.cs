22227b29101ab5bbb52c38a732b49873b036f102
Duarte, Gabriel <grd1996@gmail.com>
2026-01-27 12:22:28 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System
+
+namespace C_sharp_basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World! These are my first steps with C# into .NET");
+
+            byte number;
+            Console.WriteLine(number);
+        }
+    }
+}
