3d50872a036a1f0d5d6b6c62a7881b8a98c16f03
Duarte, Gabriel <grd1996@gmail.com>
2026-01-28 10:56:09 -0300

    playing with data types

--- Program.cs
+++ Program.cs
@@ -7,7 +7,18 @@
             Console.WriteLine("Hello, World! These are my first steps with C# into .NET");
 
             byte number = 12;
+            int count = 10;
+            float totalPrice = 20.95f;
+            char character = 'A';
+            string firstName = "Gabriel";
+            bool isWorking = true;
             Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
+            Console.WriteLine();
         }
     }
 }
