05ca1212415e323ccda4f07bfa2c0044c9770643
Duarte, Gabriel <grd1996@gmail.com>
2026-02-06 10:28:46 -0300

    playing with if/else and ternary operators

--- MainProgram.cs
+++ MainProgram.cs
@@ -7,10 +7,36 @@ namespace C_sharp_basics
     {
         static void Main(string[] args)
         {
+            var number1 = 10;
+            if (number1 > 5)
+            {
+                Console.WriteLine("var number1 which equals to " + number1 + " is greater than 5");
+            }
+            else
+            {
+                Console.WriteLine("var number1 which equals to " + number1 + "is less than or equal to 5");
+            }
 
+            bool isRaining = true;
+            if (isRaining)
+            {
+                Console.WriteLine("It is raining outside. Don't forget to take an umbrella!");
+            }
+            else
+            {
+                Console.WriteLine("It is not raining outside. Enjoy your day!");
+            }
 
+            bool isGolden = true;
+            float price;
+            if (isGolden)
+                price = 1000.50f;
+            else
+                price = 500.25f;
+            Console.WriteLine(price);
+
+            float price2 = isGolden ? 1000.50f : 500.25f; //using ternary operator to assign value to price2 based on the condition of isGolden
+            Console.WriteLine(price2);
         }
-
-
     }
 }
