73a5d0f6b5fe9c9e07a77bc59faa755dcff9167e
Duarte, Gabriel <grd1996@gmail.com>
2026-02-08 10:21:28 -0300

    using switch for the frist time + doing some exercises

--- MainProgram.cs
+++ MainProgram.cs
@@ -1,5 +1,6 @@
 ﻿using C_sharp_basics.Math; //importing the Math namespace to use the Calculator class
-using C_sharp_basics.Vehicle; //importing the Vehicle namespace to use the Car struct
+using C_sharp_basics.Vehicle;
+using Conditionals; //importing the Conditionals namespace to use the Season enum
 
 namespace C_sharp_basics
 {
@@ -35,8 +36,61 @@ namespace C_sharp_basics
                 price = 500.25f;
             Console.WriteLine(price);
 
-            float price2 = isGolden ? 1000.50f : 500.25f; //using ternary operator to assign value to price2 based on the condition of isGolden
+            //using ternary operator to assign value to price2 based on the condition of isGolden
+            float price2 = isGolden ? 1000.50f : 500.25f; 
             Console.WriteLine(price2);
+
+            //Now we'll use Season enum here to play with "switch"
+            var season = Season.Autumn;
+
+            switch (season)
+            {
+                case Season.Autumn:
+                    Console.WriteLine("It's a beautiful Season: Autumn");
+                    break;
+
+                    case Season.Summer:
+                    Console.WriteLine("U ready to carnival and get a nice color?");
+                    break;
+
+                    case Season.Winter:
+                    case Season.Spring:
+                    Console.WriteLine("You can also use two options to return the same value!");
+                    break;
+
+                default:
+                    Console.WriteLine("I don't understand the reason");
+                    break;
+
+            }
+
+            //Doing the exercises now
+
+            //Makes the user writes a valid number between 1 and 10, if the user writes an invalid number, it will show an error message
+            Console.WriteLine("Please enter a number between 1 and 10:");
+            string userInput = Console.ReadLine();
+            if( int.TryParse(userInput, out int validInputNumber) && validInputNumber >= 1 && validInputNumber <= 10)
+            {
+                Console.WriteLine("Thank you for entering a valid number: " + validInputNumber);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a number between 1 and 10.");
+            }
+
+            //Takes two numbers from the user and shows the result of addition:
+            Console.WriteLine("Please enter the first number:");
+            string firstInput = Console.ReadLine();
+            int convertedFirstInput = Convert.ToInt32(firstInput);
+            Console.WriteLine("Please enter the second number:");
+            string secondInput = Console.ReadLine();
+            int convertedSecondInput = Convert.ToInt32(secondInput);
+            Console.WriteLine("The sum is: " + (convertedFirstInput + convertedSecondInput));
+
+
+
         }
+
+
     }
 }
---
+++ Season.cs
@@ -0,0 +1,10 @@
+﻿namespace Conditionals
+{
+    public enum Season //let's simulate a database, using enum:
+    {
+        Spring,
+        Summer,
+        Autumn,
+        Winter
+    }
+}
