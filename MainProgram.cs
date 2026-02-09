e16ca3e1eafae0fef91f19b4b251fc6ef2b9cf66
Duarte, Gabriel <grd1996@gmail.com>
2026-02-09 14:32:10 -0300

    Finished first control flow exercises

--- MainProgram.cs
+++ MainProgram.cs
@@ -1,6 +1,7 @@
 ﻿using C_sharp_basics.Math; //importing the Math namespace to use the Calculator class
 using C_sharp_basics.Vehicle;
-using Conditionals; //importing the Conditionals namespace to use the Season enum
+using Conditionals;
+using System.ComponentModel.Design; //importing the Conditionals namespace to use the Season enum
 
 namespace C_sharp_basics
 {
@@ -37,7 +38,7 @@ namespace C_sharp_basics
             Console.WriteLine(price);
 
             //using ternary operator to assign value to price2 based on the condition of isGolden
-            float price2 = isGolden ? 1000.50f : 500.25f; 
+            float price2 = isGolden ? 1000.50f : 500.25f;
             Console.WriteLine(price2);
 
             //Now we'll use Season enum here to play with "switch"
@@ -49,12 +50,12 @@ namespace C_sharp_basics
                     Console.WriteLine("It's a beautiful Season: Autumn");
                     break;
 
-                    case Season.Summer:
+                case Season.Summer:
                     Console.WriteLine("U ready to carnival and get a nice color?");
                     break;
 
-                    case Season.Winter:
-                    case Season.Spring:
+                case Season.Winter:
+                case Season.Spring:
                     Console.WriteLine("You can also use two options to return the same value!");
                     break;
 
@@ -69,7 +70,7 @@ namespace C_sharp_basics
             //Makes the user writes a valid number between 1 and 10, if the user writes an invalid number, it will show an error message
             Console.WriteLine("Please enter a number between 1 and 10:");
             string userInput = Console.ReadLine();
-            if( int.TryParse(userInput, out int validInputNumber) && validInputNumber >= 1 && validInputNumber <= 10)
+            if (int.TryParse(userInput, out int validInputNumber) && validInputNumber >= 1 && validInputNumber <= 10)
             {
                 Console.WriteLine("Thank you for entering a valid number: " + validInputNumber);
             }
@@ -87,10 +88,59 @@ namespace C_sharp_basics
             int convertedSecondInput = Convert.ToInt32(secondInput);
             Console.WriteLine("The sum is: " + (convertedFirstInput + convertedSecondInput));
 
+            //Ask the user to enter the width and height of an image. Then tell if the image is a landscape or portrait
+
+            Console.WriteLine("To tell if the image is a landscape or portrait, Please, enter the width:");
+            string userWidthInput = Console.ReadLine();
+            int convertedWidth = Convert.ToInt32(userWidthInput);
+            Console.WriteLine("Now, inserte the height:");
+            string userHeightInput = Console.ReadLine();
+            int convertedHeight = Convert.ToInt32(userHeightInput);
+
+            if (convertedWidth > convertedHeight)
+            {
+                Console.WriteLine("The image is a landscape");
+
+            }
+            else if (convertedWidth < convertedHeight)
+            {
+                Console.WriteLine("The image is a portrait");
+            }
+            else
+            { Console.WriteLine("This is a square image. Not a landscape neither a portrait.");
+            }
+
+            //Your job is to write a program for a speed camera. For simplicity, ignore the details such as camera, sensors, etc and focus purely on the logic.
+            //Write a program that asks the user to enter the speed limit. Once set, the program asks for the speed of a car.
+            //If the user enters a value less than the speed limit, program should display Ok on the console.
+            //If the value is above the speed limit, the program should calculate the number of demerit points.
+            //For every 5km/hr above the speed limit, 1 demerit points should be incurred and displayed on the console.
+            //If the number of demerit points is above 12, the program should display License Suspended.
+
+            Console.WriteLine("Please, enter the speed limit in this range of the road:");
+            int speedLimit = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The speed limit is " + speedLimit + "kilometers per hour. Ok, now please insert the speed of the car:");
+            int carSpeed = Convert.ToInt32(Console.ReadLine());
+
+            if (carSpeed <= speedLimit)
+            {
+                Console.WriteLine("Ok");
+            }
+            else
+            {
+                int excessSpeed = carSpeed - speedLimit;
+                int demeritPoints = excessSpeed / 5;
+
+                Console.WriteLine("Demerit points: " + demeritPoints);
+
+                if (demeritPoints >=12)
+                    {
+                    Console.WriteLine("Perdeu playboy, passa a carteira");
+                    }
+            }
+           
 
 
         }
-
-
     }
 }
